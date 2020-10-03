9db35acbb7912a7f1572b44efe470978bad46a7b
sublethaljpg <62752279+sublethaljpg@users.noreply.github.com>
2020-10-03 17:57:56 +0800

    mobile

--- style.js
+++ style.js
@@ -2,8 +2,6 @@ var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 if (isMobile) {
   console.log("User is using a mobile device.");
-  $("body").css("height", "100vh");
-  $("#calc").css("height", "100%");
   $("#calc").css("background-color", "yellow");
 } else {
   console.log("User is not using a mobile device.");
